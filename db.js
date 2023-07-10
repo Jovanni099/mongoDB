1dde29c107a83e555133495735703ccd55501261
Jovanni099 <jovanni099tmb@gmail.com>
2023-07-10 19:44:08 +0600

    Установил монго

---
+++ db.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require('mongodb');
+
+
+const URL = 'mongodb+srv://jovanni099tmb:AZzLsJGPaeOS59Ck@jovannimongodatabase.dqesfcv.mongodb.net/moviebox'
+
+let dbConnection;
+
+module.exports = {
+    connectToDb: (cb) => {
+        MongoClient
+            .connect(URL)
+            .then((client) => {
+                console.log('Connected to MongoDB');
+                dbConnection = client.db();
+                return cb();
+            })
+            .catch((err) => {
+                return cb(err);
+            })
+
+    },
+    getDb: () => dbConnection,
+
+}
+
+// const client = new MongoClient('mongodb://localhost:27017', { monitorCommands: true });
+
+// client.on('commandStarted', started => console.log(started));
+// client.db().collection('pets');
+// await client.insertOne({ name: 'spot', kind: 'dog' });
